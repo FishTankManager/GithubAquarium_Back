3346689f2c6efc54f9d7f5d9544fda3f4c1927e8
Imggaggu <ggaggu@snu.ac.kr>
2025-10-22 17:59:30 +0900

    [Feat] initialize manager app

---
+++ apps/manager/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class ManagerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = "apps.manager"
+    label = "manager"
+    verbose_name = "Manager (운영진)" # 어드민 사이드바
+    
+    def ready(self):
+        from . import signals 
