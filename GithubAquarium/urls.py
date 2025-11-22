cde1f43263101b50cc8ff1b9b8e1932521c64df7
Imggaggu <ggaggu@snu.ac.kr>
2025-11-22 15:20:09 +0900

    [Fix] add media urls

--- GithubAquarium/urls.py
+++ GithubAquarium/urls.py
@@ -10,6 +10,8 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import GitHubLogin
 from .webhook_views import GitHubWebhookView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from dj_rest_auth.views import LogoutView, UserDetailsView
 from dj_rest_auth.jwt_auth import get_refresh_view
@@ -63,3 +65,5 @@ urlpatterns = [
     # --- DRF Auth ---
     path('api-auth/', include('rest_framework.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
